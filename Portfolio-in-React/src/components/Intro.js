357e6d5073825ef80a41bf62909a9544328a2bc7
Estehan198 <estehsanul@gmail.com>
2025-07-30 18:38:20 +0500

    Updates intro..

--- Portfolio-in-React/src/components/Intro.js
+++ Portfolio-in-React/src/components/Intro.js
@@ -81,7 +81,7 @@ const Intro = () => {
                 <Text>
                     <h1>Hi,</h1>
                     <h3>I'm Babu Coder</h3>
-                    <h6>I design and Code simple yet beautiful websites.</h6>
+                    <h6>I design and Code simple yet beautiful websites...</h6>
                 </Text>
             </SubBox>
             <SubBox>
